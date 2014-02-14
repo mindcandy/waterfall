7dcbe214f47a27328b8f66c78016642ac9d4e2ea
Kevin Schmidt <kevin.schmidt@mindcandy.com>
2014-02-14 12:17:39 +0000

    * basic job service

---
+++ src/main/scala/com/mindcandy/waterfall/service/JobService.scala
@@ -0,0 +1,9 @@
+package com.mindcandy.waterfall.service
+
+import spray.routing.HttpService
+import com.typesafe.scalalogging.slf4j.Logging
+
+
+trait JobService extends HttpService with Logging {
+
+}
