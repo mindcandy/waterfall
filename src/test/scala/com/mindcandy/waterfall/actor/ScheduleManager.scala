0b9edf48ab41e4cd5426e000f9e91b2056af2bb7
Kevin Schmidt <kevin.schmidt@mindcandy.com>
2014-05-14 20:46:06 +0100

    * adding unit test for cron schedule

--- src/test/scala/com/mindcandy/waterfall/actor/ScheduleManager.scala
+++ src/test/scala/com/mindcandy/waterfall/actor/ScheduleManager.scala
@@ -10,17 +10,23 @@ import akka.actor.ActorRef
 import scala.util.Success
 import com.mindcandy.waterfall.actor.ScheduleManager.CheckJobs
 import com.mindcandy.waterfall.actor.JobDatabaseManager.GetSchedule
+import com.mindcandy.waterfall.actor.Protocol.DropJobList
+import com.mindcandy.waterfall.actor.Protocol.DropJob
+import com.github.nscala_time.time.Imports._
+import scala.language.postfixOps
+import com.mindcandy.waterfall.actor.DropSupervisor.StartJob
+import scala.concurrent.duration._
 
 class ScheduleManagerSpec extends TestKit(ActorSystem("ScheduleManagerSpec")) with SpecificationLike with After with NoTimeConversions {
   override def is = s2"""
-     ScheduleManager should
-       should contact job database on check jobs message $checkJob
+    ScheduleManager should
+      should contact job database on check jobs message $checkJobs
+      should schedule one job if one is send from the databaseManager $scheduleOneJob
   """
 
   override def after: Any = TestKit.shutdownActorSystem(system)
 
-  def checkJob = {
-    val dropUID = "test1"
+  def checkJobs = {
     val probe: TestProbe = TestProbe()
     val databaseManager: TestProbe = TestProbe()
     val dropSupervisor: TestProbe = TestProbe()
@@ -29,5 +35,20 @@ class ScheduleManagerSpec extends TestKit(ActorSystem("ScheduleManagerSpec")) wi
 
     probe.send(actor, request)
     databaseManager.expectMsgClass(classOf[GetSchedule]) must_== GetSchedule()
+  }
+
+  def scheduleOneJob = {
+    val probe: TestProbe = TestProbe()
+    val databaseManager: TestProbe = TestProbe()
+    val dropSupervisor: TestProbe = TestProbe()
+    val actor: ActorRef = system.actorOf(ScheduleManager.props(databaseManager.ref, dropSupervisor.ref, TestWaterfallDropFactory))
+    val currentTime = DateTime.now + Period.seconds(5)
+    
+    val dropJob = DropJob("EXRATE", "Exchange Rate", true, s"${currentTime.secondOfMinute.getAsString} ${currentTime.minuteOfHour.getAsString} ${currentTime.hourOfDay.getAsString} * * ?")
+    val request = DropJobList(List(dropJob))
+
+    probe.send(actor, request)
+    println("start job: " + StartJob(dropJob))
+    dropSupervisor.expectMsgClass(FiniteDuration(10, SECONDS), classOf[StartJob]) must_== StartJob(dropJob)
   }
 }
