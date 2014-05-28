807f331a11a7cde61d6c2f0df2abd2b2b60810f2
Nelson Correia <nelson.correia@mindcandy.com>
2014-05-28 16:23:07 +0100

    Add unit test to ensure the worker actor is stopped.

--- src/test/scala/com/mindcandy/waterfall/actor/DropWorkerSpec.scala
+++ src/test/scala/com/mindcandy/waterfall/actor/DropWorkerSpec.scala
@@ -3,20 +3,20 @@ package com.mindcandy.waterfall.actor
 import org.specs2.specification.After
 import akka.testkit.TestKit
 import org.specs2.time.NoTimeConversions
-import akka.actor.ActorSystem
+import akka.actor.{Terminated, ActorSystem, ActorRef, Props}
 import org.specs2.SpecificationLike
 import akka.testkit.TestProbe
-import akka.actor.ActorRef
-import akka.actor.Props
 import scala.util.Success
 import com.mindcandy.waterfall.actor.DropWorker.RunDrop
 import com.mindcandy.waterfall.actor.DropSupervisor.JobResult
+import scala.concurrent.duration._
 
 
 class DropWorkerSpec extends TestKit(ActorSystem("DropWorkerSpec")) with SpecificationLike with After with NoTimeConversions {
   override def is = s2"""
     DropWorker should
       run a drop and return success $runDrop
+      stop after running a job $stopActor
   """
 
   override def after: Any = TestKit.shutdownActorSystem(system)
@@ -29,5 +29,20 @@ class DropWorkerSpec extends TestKit(ActorSystem("DropWorkerSpec")) with Specifi
 
     probe.send(actor, request)
     probe.expectMsgClass(classOf[JobResult]).result must_== Success()
+  }
+
+  def stopActor = {
+    val dropUID = "test1"
+    val probe: TestProbe = TestProbe()
+    val actor: ActorRef = system.actorOf(DropWorker.props)
+    val request = RunDrop(dropUID, TestWaterfallDropFactory.getDropByUID(dropUID).get)
+    probe.watch(actor)
+
+    probe.send(actor, request)
+    probe.expectMsgClass(classOf[JobResult])
+    probe.expectTerminated(actor, FiniteDuration(5, SECONDS)) match {
+      case Terminated(actor) => success
+      case _ => failure
+    }
   }
 }
