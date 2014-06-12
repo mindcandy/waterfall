61468259f774f3bc5c520e9da7a29d90a9643e82
Kevin Schmidt <kevin.schmidt@mindcandy.com>
2014-06-12 20:15:29 +0100

    * switching spray to listen on all ips * rework RedshiftIO to have both a source and a sink who work symmetrically * make the PassThroughPattern less restrictive

---
+++ src/it/scala/com/mindcandy/waterfall/io/RedshiftIOSpec.scala
@@ -0,0 +1,84 @@
+package com.mindcandy.waterfall.io
+
+import com.mindcandy.waterfall.drop.PassThroughWaterfallDrop
+import com.mindcandy.waterfall.intermediate.{ S3IntermediateConfig, FileIntermediate, S3Intermediate }
+import com.mindcandy.waterfall._
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class RedshiftIOSpec extends Specification {
+
+  val s3Url = "FILLIN"
+  val awsAccessKey = "FILLIN"
+  val awsSecretKey = "FILLIN"
+  val bucketName = "FILLIN"
+  val keyPrefix = "test-file"
+  val redshiftUrl = "FILLIN"
+  val redshiftUser = "FILLIN"
+  val redshiftPass = "FILLIN"
+  val tableName = "test_table"
+  val query =
+    """
+      |SELECT * FROM test_table
+    """.stripMargin
+
+  case class TestData(userID: String, otherID: String, unifiedID: String, providerID: String, providerLogin: String, timestamp: String)
+
+  object TestData extends IntermediateFormatCompanion[TestData] {
+    object format extends IntermediateFormat[TestData] {
+      def convertTo(input: Seq[String]) = {
+        TestData(
+          input(0),
+          input(1),
+          input(2),
+          input(3),
+          input(4),
+          input(5)
+        )
+      }
+
+      def convertFrom(input: TestData) = Seq[String](
+        input.userID,
+        input.otherID,
+        input.unifiedID,
+        input.providerID,
+        input.providerLogin,
+        input.timestamp
+      )
+    }
+
+    val redshiftColumns = List("user_id", "other_id", "unified_id", "provider_id", "provider_login", "timestamp")
+  }
+
+  case class TestRedshiftDrop()(implicit val sharedIntermediateFormat: IntermediateFormat[TestData]) extends PassThroughWaterfallDrop[TestData] {
+    override val sharedIntermediate = S3Intermediate[TestData](s3Url, awsAccessKey, awsSecretKey, bucketName, keyPrefix)
+
+    override def source = RedshiftIOSource[TestData](
+      RedshiftIOSourceConfig(
+        redshiftUrl,
+        redshiftUser,
+        redshiftPass,
+        query
+      )
+    )
+
+    override def sink = RedshiftIOSink[TestData](
+      RedshiftIOSinkConfig(
+        redshiftUrl,
+        redshiftUser,
+        redshiftPass,
+        tableName,
+        Some(TestData.redshiftColumns)
+      )
+    )
+  }
+
+  "RedshiftIOSource" should {
+    "should work for test table" in {
+      val result = TestRedshiftDrop().run
+      result must beSuccessfulTry
+    }
+  }
+}
---
+++ src/main/scala/com/mindcandy/waterfall/io/RedshiftIO.scala
@@ -0,0 +1,56 @@
+package com.mindcandy.waterfall.io
+
+import com.mindcandy.waterfall._
+import scala.slick.session.Database
+import scala.slick.session.Database.threadLocalSession
+import scala.slick.jdbc.StaticQuery
+import com.typesafe.scalalogging.slf4j.Logging
+import scala.util.Try
+import com.mindcandy.waterfall.intermediate.S3Intermediate
+
+trait RedshiftIOConfig extends IOConfig {
+  def driver: String = "org.postgresql.Driver"
+  def username: String
+  def password: String
+}
+case class RedshiftIOSourceConfig(url: String, username: String, password: String, query: String) extends RedshiftIOConfig
+case class RedshiftIOSinkConfig(url: String, username: String, password: String, tableName: String, columnNames: Option[List[String]] = None) extends RedshiftIOConfig
+
+case class RedshiftIOSource[A](config: RedshiftIOSourceConfig, s3Config: Option[S3IOConfig] = None)
+    extends IOSource[A]
+    with Logging {
+
+  def retrieveInto[I <: Intermediate[A]](intermediate: I)(implicit format: IntermediateFormat[A]): Try[Unit] = {
+    val s3Intermediate = intermediate match {
+      case i: S3Intermediate[A] => i
+      case _ => throw new IllegalArgumentException("currently only S3Intermediate supported")
+    }
+    val combinedS3Url = s"s3://${s3Intermediate.bucketName}/${s3Intermediate.datedKeyPrefix}/"
+    logger.info(s"Copying Redshift query into S3 with url ${combinedS3Url} in db ${config.url}")
+    Try(Database.forURL(config.url, driver = config.driver, user = config.username, password = config.password).withSession {
+      StaticQuery.updateNA(
+        s"UNLOAD ('${config.query.replace("'", "\\\'")}') TO '${combinedS3Url}' CREDENTIALS 'aws_access_key_id=${s3Intermediate.awsAccessKey};aws_secret_access_key=${s3Intermediate.awsSecretKey}' NULL AS '\\N' DELIMITER '\\t'"
+      ).execute
+    })
+  }
+}
+
+case class RedshiftIOSink[A](config: RedshiftIOSinkConfig, s3Config: Option[S3IOConfig] = None)
+    extends IOSink[A]
+    with Logging {
+
+  def storeFrom[I <: Intermediate[A]](intermediate: I)(implicit format: IntermediateFormat[A]) = {
+    val s3Intermediate = intermediate match {
+      case i: S3Intermediate[A] => i
+      case _ => throw new IllegalArgumentException("currently only S3Intermediate supported")
+    }
+    val combinedS3Url = s"s3://${s3Intermediate.bucketName}/${s3Intermediate.datedKeyPrefix}"
+    val columns = config.columnNames.map("(" + _.mkString(",") + ")").getOrElse("")
+    logger.info(s"Copying S3 data from ${combinedS3Url} into Redshift table ${config.tableName} in db ${config.url}")
+    Try(Database.forURL(config.url, driver = config.driver, user = config.username, password = config.password).withSession {
+      StaticQuery.updateNA(
+        s"COPY ${config.tableName} ${columns} FROM '${combinedS3Url}' CREDENTIALS 'aws_access_key_id=${s3Intermediate.awsAccessKey};aws_secret_access_key=${s3Intermediate.awsSecretKey}' NULL AS '\\N' DELIMITER '\\t'"
+      ).execute
+    })
+  }
+}
