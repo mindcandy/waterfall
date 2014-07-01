be6213379ff123e5141b9c16a040fe41c6100982
Martin Carolan <martin@mindcandy.com>
2014-07-01 09:47:57 +0100

    Add CassandraIOSpec

---
+++ src/it/scala/com/mindcandy/waterfall/io/CassandraIOSpec.scala
@@ -0,0 +1,17 @@
+package com.mindcandy.waterfall.io
+
+import org.specs2._
+
+class CassandraIOSpec extends Specification {
+
+  def is = s2"""
+                CassandraIOSpec
+
+                  works     $works
+    """
+
+
+
+  def works = true must beTrue
+
+}
