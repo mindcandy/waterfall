7900da9a0e3ac2ed43ea89293b87312985eaeddc
Kevin Schmidt <kevin.schmidt@mindcandy.com>
2014-07-31 19:42:10 +0100

    * fixing S3IO for lots of keys and large files

--- src/main/scala/com/mindcandy/waterfall/io/S3IO.scala
+++ src/main/scala/com/mindcandy/waterfall/io/S3IO.scala
@@ -1,16 +1,20 @@
 package com.mindcandy.waterfall.io
 
 import java.io.{ SequenceInputStream, InputStream, InputStreamReader, BufferedReader }
+import com.amazonaws.services.s3.model.ObjectListing
 import resource._
+import scala.annotation.tailrec
 import scala.collection.JavaConverters._
-import scala.util.{ Try, Failure }
+import scala.util.{ Success, Try, Failure }
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.mindcandy.waterfall.RowSeparator.RowSeparator
 import com.mindcandy.waterfall._
 import com.mindcandy.waterfall.RowSeparator._
 
-case class S3IOConfig(url: String, awsAccessKey: String, awsSecretKey: String, bucketName: String, keyPrefix: String) extends IOConfig
+case class S3IOConfig(url: String, awsAccessKey: String, awsSecretKey: String, bucketName: String, keyPrefix: String) extends IOConfig {
+  override def toString = "S3IOConfig(%s, %s, %s)".format(url, bucketName, keyPrefix)
+}
 
 case class S3IO[A <: AnyRef](config: S3IOConfig, val keySuffix: Option[String] = None, val columnSeparator: Option[String] = Option("\t"), val rowSeparator: RowSeparator = NewLine)
     extends IOSource[A]
@@ -19,36 +23,35 @@ case class S3IO[A <: AnyRef](config: S3IOConfig, val keySuffix: Option[String] =
 
   def retrieveInto[I <: Intermediate[A]](intermediate: I)(implicit format: IntermediateFormat[A]) = {
     val keyPrefix = config.keyPrefix + keySuffix.getOrElse("")
-    val computedKeys = amazonS3Client.map {
-      _.listObjects(config.bucketName, keyPrefix).getObjectSummaries.asScala.toList.map(_.getKey)
-    }
-    val bufferedReader = amazonS3Client.flatMap { s3Client =>
-      computedKeys.flatMap {
-        case Nil => Failure(new Exception(s"No keys found in ${config.bucketName} for key prefix ${keyPrefix}"))
-        case keys => Try {
-          logger.info(s"Starting stream from S3 with endpoint ${config.url} using ${config.bucketName} and keys ${keys}")
-          val inputStreams = keys.map(s3Client.getObject(config.bucketName, _).getObjectContent())
-          new BufferedReader(new InputStreamReader(inputStreams.reduceLeft[InputStream] { (input1, input2) =>
-            new SequenceInputStream(input1, input2)
-          }))
+    val computedKeys = amazonS3Client.map { s3Client => getObjects(Nil, s3Client.listObjects(config.bucketName, keyPrefix), s3Client) }
+    computedKeys.flatMap { keys =>
+      logger.info(s"Starting stream from S3 with endpoint ${config.url} using ${config.bucketName} and keys ${keys}")
+      keys.foldLeft(Try(())) { (result, key) =>
+        result.flatMap { _ =>
+          amazonS3Client.flatMap { s3Client =>
+            val inputContent = for {
+              reader <- managed(new BufferedReader(new InputStreamReader(s3Client.getObject(config.bucketName, key).getObjectContent())))
+            } yield {
+              val rawData = Iterator.continually {
+                Option(reader.readLine())
+              }.takeWhile(_.nonEmpty).flatten
+              processRowSeparator(rawData, rowSeparator)
+            }
+            inputContent.acquireFor(intermediate.write).convertToTry.map { _ =>
+              logger.info("Retrieving into %s from %s for key %s completed".format(intermediate, config, key))
+            }
+          }
         }
       }
     }
-    val inputContent = bufferedReader.map { bufReader =>
-      for {
-        reader <- managed(bufReader)
-      } yield {
-        val rawData = Iterator.continually {
-          Option(reader.readLine())
-        }.takeWhile(_.nonEmpty).flatten
-        processRowSeparator(rawData, rowSeparator)
-      }
-    }
+  }
 
-    inputContent.flatMap { resource =>
-      resource.acquireFor(intermediate.write).convertToTry.map { _ =>
-        logger.info("Retrieving into %s from %s completed".format(intermediate, config))
-      }
+  @tailrec
+  private[this] def getObjects(acc: List[String], listing: ObjectListing, s3Client: AmazonS3Client): List[String] = {
+    val keys = listing.getObjectSummaries.asScala.toList.map(_.getKey)
+    listing.isTruncated match {
+      case true => getObjects(acc ::: keys, s3Client.listNextBatchOfObjects(listing), s3Client)
+      case false => acc ::: keys
     }
   }
 
